2f20111b341514afbbec92ce0649a566b68efebc
Allan <allanwu12321@gmail.com>
2024-08-01 17:43:39 -0700

    DOM Querying Submission

--- dom-querying/dom-querying-exercise/script.js
+++ dom-querying/dom-querying-exercise/script.js
@@ -1,1 +1,13 @@
 // Dom Querying Exercise here
+
+let h1 = $("#main-title");
+let texts = $(".text");
+let ul = $("#list");
+let firstLi = $("#item1");
+let allLi = $("li");
+
+console.log(h1);
+console.log(texts);
+console.log(ul);
+console.log(firstLi);
+console.log(allLi);
